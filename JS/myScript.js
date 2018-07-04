06dcd10c2c637a4257755892deb3bc67c4ff2f27
admin <qwdfgferty@sdvjdv.sdhk>
2018-07-04 19:12:11 +0200

    Complete Overhaul
    Redesigned pretty much everything.

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,128 +1,44 @@
-var panels = document.getElementsByClassName("panel");
+/*jslint devel: true */
+/*jslint white: true */
+/*global window */
 
-var h1flag = false;
-var h1content;
+(function () {
+	"use strict";
+	//get a random Integer
+	function getRandomIntInclusive(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 
-function randomize()
-{
-  //load random animation
-  document.getElementById("anim-object").setAttribute('data', 'animations/anim'+getRandomIntInclusive(1,4)+'.html');
-  
-  var bggradcolors = tinycolor(tinycolor.random()).triad();
-  
-  $('#bggrad').css({
-    'background': '-webkit-linear-gradient(45deg,'+ bggradcolors[0] + ","+ bggradcolors[1] +',' + bggradcolors[2] + ')',
-    'background-size': '600% 600%'
-  });
-  
-  for (i = 0; i < panels.length; i++) 
-  {
-    // Pick a random color
-    //var hexColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-    var rgbColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' 
-                          + (Math.floor(Math.random() * 256)) + ',' 
-                          + (Math.floor(Math.random() * 256)) + ')';
-    panels[i].style.backgroundColor = rgbColor;
-  }
-  
-  //adjustments
-  var mainclr = panels[0].style.backgroundColor; 
-  panels[1].style.backgroundColor = mainclr;
-  panels[2].style.backgroundColor = tinycolor(mainclr).complement();
-  panels[3].style.backgroundColor = tinycolor(panels[2].style.backgroundColor).darken();
-//  panels[4].style.backgroundColor = tinycolor(panels[2].style.backgroundColor).darken();
-//  $('#popup').css("backgroundColor", panels[4].style.backgroundColor);
-    
-  //font color
-  for (i = 0; i < panels.length; i++) 
-  {
-    if(tinycolor(panels[i].style.backgroundColor).isLight())
-    {
-      panels[i].style.color='black';
-    }
-  }
-  
-  if(tinycolor(panels[3].style.backgroundColor).isLight())
-    {
-      $('a').css("color","black");
-    }
-  else
-    {
-      $('a').css("color", "white");
-    }
-  
-  window.setInterval(changeH1, 6000);
-  h1content = document.getElementById("changing_h").innerHTML;
-  $(".panel").addClass("panelchanged");
-}
+	function randomize()
+	{
+		//load random animation
+		window.document.getElementById("anim-object").setAttribute('data', 'animations/anim'+getRandomIntInclusive(1,4)+'.html');
+		
+		//random color
+		var colorSchemes = [
+						["white", "#eb0a72", "#0f69e3"],
+						["#240041", "#900048", "#ff4057"],
+						["white", "#FEDF03", "#0BB4C1"]
+			];
+		
+		var rdmInt = getRandomIntInclusive(0,2);
+		window.document.documentElement.style.setProperty('--bg', colorSchemes[rdmInt][0]);
+		window.document.documentElement.style.setProperty('--c3', colorSchemes[rdmInt][1]);
+		window.document.documentElement.style.setProperty('--c4', colorSchemes[rdmInt][2]);
+		
+	}
 
-function changeH1() {
-  var h1text = document.getElementById("changing_h");
-  if(!h1flag)
-  {
-    h1text.style.opacity = '0';
-    window.setTimeout(textc1, 1000);
-    h1flag = true;
-  }
-  else
-  {
-    h1text.style.opacity = '0';
-    window.setTimeout(textc2, 1000);
-    h1flag = false;
-  }
-}
+	//on page load event
+	window.document.addEventListener("DOMContentLoaded", function() {
+		randomize();
+//		window.document.getElementById('anim-click').onclick = function() { randomize(); };
+	});
 
-function textc1() {
-  var h1text = document.getElementById("changing_h");
-  h1text.innerHTML = "Try refreshing the page!";
-  h1text.style.opacity = '1';
-}
-
-function textc2() {
-  var h1text = document.getElementById("changing_h");
-  h1text.innerHTML = h1content;
-  h1text.style.opacity = '1';
-}
-
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-}
+}());
 
 
-$(document).ready(function() {
-  
-//  $(".link").hover(function(){
-//    $(this).animate({fontSize: "1.2em"},100);
-//  }, function(){
-//    $(this).animate({fontSize: "1em"},100);
-//  });   
-});
 
-/*
-    $( ".d" ).one( "click", function() {
-      $( ".c" ).animate({height:0},1000);
-      
-      $( ".c" ).queue(function (next) { 
-        $(this).css('display', 'none'); 
-        next();
-      });
-      
-      $(".wrapper").css( "grid-template-rows", "30% 0% 60% 10%" );
-    });
-    */
 
-/*
-function changeStuff() {
-  var stuff = document.getElementsByClassName("d");
-  stuff[0].classList.add("active");
 
-  for (i = 0; i < panels.length; i++) 
-  {
-    if( i!= 3) {
-      panels[i].style.backgroundColor = tinycolor(panels[i].style.backgroundColor).brighten(50);
-    } 
-  }
-}
-*/
